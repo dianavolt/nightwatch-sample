8a3ac9b98d74d7769f3ad99682af90ec6f50dce2
dianavolt <diana.voltchok@telus.com>
2020-12-30 12:16:35 -0800

    add sample test

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,3 +1,8 @@
 module.exports = {
-    
+    'first test case'(browser) {
+        browser
+            .url('https://stackoverflow.com/')
+            .waitForElementVisible('#for-developers')
+            .assert.containsText('#for-developers', 'For developers, by developers')
+    }
 }
